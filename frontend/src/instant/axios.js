cbec8fd3dba0ed1880e7570e1038d173a4bb332a
Deepti Lodhi <lodhideepti39@gmail.com>
2025-05-09 15:48:28 +0530

    axios

--- frontend/src/instant/axios.js
+++ frontend/src/instant/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080', // Backend URL
+  baseURL: 'https://saadidukan-2.onrender.com', // Backend URL
   headers: {
     'Content-Type': 'application/json',
   },
